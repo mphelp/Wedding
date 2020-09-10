0900df8337843402c3651ccc1e6b427bf35542d4
Matt Phelps <m231174@8451.com>
2020-09-10 02:07:04 -0500

    Bump opine to fix dep

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 export { default as React } from "https://dev.jspm.io/react@16.13.1";
 export { default as ReactDOMServer } from "https://dev.jspm.io/react-dom@16.13.1/server";
-export { opine, serveStatic } from "https://deno.land/x/opine@0.14.0/mod.ts";
+export { opine, serveStatic } from "https://deno.land/x/opine@0.21.3/mod.ts";
 export { default as Vert } from "https://dev.jspm.io/react-vertical-timeline-component@2.6.2";
 export { join, dirname } from "https://deno.land/std@0.66.0/path/mod.ts";
 
