4e38249598cd1561764adb439dbeeba82b50b914
Matt Phelps <m231174@8451.com>
2020-09-10 02:17:42 -0500

    Proper port setup for app. Also... THE APP DEPLOYS YAHOO

--- deps.ts
+++ deps.ts
@@ -3,6 +3,8 @@ export { default as ReactDOMServer } from "https://dev.jspm.io/react-dom@16.13.1
 export { opine, serveStatic } from "https://deno.land/x/opine@0.21.3/mod.ts";
 export { default as Vert } from "https://dev.jspm.io/react-vertical-timeline-component@2.6.2";
 export { join, dirname } from "https://deno.land/std@0.66.0/path/mod.ts";
+export { parse } from "https://deno.land/std/flags/mod.ts";
+
 
 
 //export { default as HomeWork } from "https://dev.jspm.io/@material-ui/icons/HomeWork";
