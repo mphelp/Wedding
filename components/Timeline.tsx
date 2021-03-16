9bcb3fa9f06e304c089713f3628efecb860ce5b0
Matt Phelps <m231174@8451.com>
2021-03-16 18:46:36 -0500

    Pinning to 2.1.5 renders right

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -1,25 +1,11 @@
 // @deno-types="https://raw.githubusercontent.com/Soremwar/deno_types/4a50660/react/v16.13.1/react.d.ts"
 import React from "https://dev.jspm.io/react@16.13.1";
 // VERTICAL TIMELINE
-import { default as Vert } from "https://dev.jspm.io/react-vertical-timeline-component@2.1";
+import { default as Vert } from "https://dev.jspm.io/react-vertical-timeline-component@2.1.5";
 //@ts-ignore
 import VerticalTimelineElement = Vert.VerticalTimelineElement;
 //@ts-ignore
 import VerticalTimeline = Vert.VerticalTimeline;
-
-
-// GOOGLE PHOTO GALLERY
-//@ts-ignore
-//import Gallery from "https://dev.jspm.io/react-photo-gallery@5.6.0";
-//@ts-ignore
-//import { default as GPhoto } from 'https://dev.jspm.io/react-google-photo@1.2.0';
-//@ts-ignore
-//import GooglePhoto = GPhoto.GooglePhoto;
-//@ts-ignore
-//console.log(Gallery)
-//@ts-ignore
-//import ImageGallery from "https://dev.jspm.io/react-image-gallery@1.0.9"
-//console.log(ImageGallery);
 
 // Story Content
 import { years } from '../story.ts'
@@ -33,12 +19,20 @@ export const Timeline = () => {
     // Could do stuff here
     return (
         <>
-            <VerticalTimelineElement
-                key={0}
-                date={"0"}
-            >
-                    <img src={images[0][0]} className="custom_img"/>
-            </VerticalTimelineElement>
+            <VerticalTimeline>
+                { years.map((year, year_i) => (
+                    <VerticalTimelineElement
+                        key={year_i}
+                        date={year}
+                        icon={icons[year_i]()}
+                        iconStyle={{ background: 'rgb(255, 255, 255)', color: '#fff', width: '60' }}
+                        dateStyle="padding-left: 15px"
+                        layout={'2-columns'}
+                    >
+                        <img src={images[year_i][0]} className="custom_img"/>
+                    </VerticalTimelineElement>
+                ))}
+            </VerticalTimeline>
         </>
     );
 }
